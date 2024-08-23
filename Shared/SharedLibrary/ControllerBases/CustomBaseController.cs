e76ae61c865a262fa6f15a7e89f6a90a833aa78d
Saadettin Ördekçi <cenbek03@hotmail.com>
2024-08-23 09:41:58 +0300

    category  and course controllers added. base controller was created for generic reposnse

---
+++ Shared/SharedLibrary/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedLibrary.Dtos;
+
+namespace SharedLibrary.ControllerBases
+{
+  public class CustomBaseController : ControllerBase
+  {
+    public IActionResult CreateActionResultInstance<T>(ResponseDto<T> response)
+    {
+      return new ObjectResult(response)
+      {
+        StatusCode = response.StatusCode
+
+
+      };
+
+    }
+  }
+}
